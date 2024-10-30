e7b0a1e85592d5a7671ac6f852f44e21b4ef8fa5
christiangubana <christiangubana@gmail.com>
2024-10-30 16:16:08 +0200

    Adding Extra Type Information to EventEmitter

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -10,7 +10,7 @@ export class UserComponent {
   @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
-  @Output() select = new EventEmitter();
+  @Output() select = new EventEmitter<string>();
 
   // avatar = input.required<string>();
   // name = input.required<string>();
