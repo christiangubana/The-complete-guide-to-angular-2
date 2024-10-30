92fa917ca79fc36ae439ebc09aa99b3b6f5f7720
christiangubana <christiangubana@gmail.com>
2024-10-30 22:47:10 +0200

    adding more component to the demo

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -12,7 +12,7 @@ import { TasksComponent } from './tasks/tasks.component';
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId = 'u1';
+  selectedUserId?: string;
 
   get clickedUser() {
     return this.users.find((user) => user.id === this.selectedUserId)!;
