11489971ff4fd8b8dd7543b2e38b1d895c075cf1
christiangubana <christiangubana@gmail.com>
2024-10-30 17:14:09 +0200

    Exercise:Create a Configure Component

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -2,17 +2,24 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-uses';
+import { TasksComponent } from './tasks/tasks.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
 
-  onSelectUser(id:string) {
-    console.log(`You select user ID ${id}`)
+  get clickedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
+
+  onClickedUser(id: string) {
+    // console.log(`You select user ID ${id}`);
+    this.selectedUserId = id;
   }
 }
