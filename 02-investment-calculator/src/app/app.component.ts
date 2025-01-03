19760440346fdd2fffa501fc0db1a2b47a311a97
christiangubana <christiangubana@gmail.com>
2025-01-03 14:21:14 +0200

    Passing data from parent to child component using Input decorator

--- 02-investment-calculator/src/app/app.component.ts
+++ 02-investment-calculator/src/app/app.component.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
 import type { InvestementInput } from './investement-input.model';
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
 export class AppComponent {
+  resultsData?: {
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+  }[]
   onCalculateInvestmentResults(data: InvestementInput) {
     const { initialInvestment, duration, expectedReturn, annualInvestment } =
       data;
@@ -31,7 +40,6 @@ export class AppComponent {
         totalAmountInvested: initialInvestment + annualInvestment * year,
       });
     }
-    console.log(annualData);
-    // return annualData;
+    this.resultsData = annualData;
   }
 }
