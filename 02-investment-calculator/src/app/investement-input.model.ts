d3085aa6803d3da11593da3103305b09bd022d5d
christiangubana <christiangubana@gmail.com>
2025-01-03 13:32:11 +0200

    Create model type

---
+++ 02-investment-calculator/src/app/investement-input.model.ts
@@ -0,0 +1,13 @@
+export interface InvestementInput {
+  initialInvestment: number;
+  duration: number;
+  expectedReturn: number;
+  annualInvestment: number;
+}
+
+// export type InvestementInput = {
+//   initialInvestment: number;
+//   duration: number;
+//   expectedReturn: number;
+//   annualInvestment: number;
+// };
